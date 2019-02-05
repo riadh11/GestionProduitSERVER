41d8c31f21ae07588a1358d0945d946e2a332ae1
Elkamel <riadhelkamel1@hotmail.fr>
2019-02-05 11:10:32 +0100

    first commit

---
+++ src/main/java/com/ProduitStockServ/Elkamel/GestionStockProdServeurApplication.java
@@ -0,0 +1,14 @@
+package com.ProduitStockServ.Elkamel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionStockProdServeurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionStockProdServeurApplication.class, args);
+	}
+
+}
+
